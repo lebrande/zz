23a171753582c02001b5ae7b0ffbdbefabd7ae95
Jakub Pusiak <jakub.pusiak@gmail.com>
2019-10-16 04:07:32 +0200

    App wrapper.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,18 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
 import { BoatData } from '../../types/BoatData';
 import Boat from '../Boat/Boat';
+import { device } from '../../styled/breakpoints';
+
+const AppWrapper = styled.div`
+  margin: 4em auto;
+  max-width: 430px;
+
+  @media ${device.laptop} {
+    max-width: 1000px;
+  }
+`;
 
 const App = () => {
   const [boats, setBoats] = useState<BoatData[]>([]);
@@ -15,14 +26,14 @@ const App = () => {
   }, []);
 
   return (
-    <>
+    <AppWrapper>
       {boats.map((boat) => (
         <Boat
           key={boat.id}
           {...boat}
         />
       ))}
-    </>
+    </AppWrapper>
   );
 };
 
--- src/components/Boat/BoatWrapper.ts
+++ src/components/Boat/BoatWrapper.ts
@@ -6,12 +6,10 @@ const BoatWrapper = styled.div`
   position: relative;
   display: flex;
   margin-bottom: 1em;
-  max-width: 430px;
   flex-direction: column;
 
   @media ${device.laptop} {
     flex-direction: row;
-    max-width: 1000px;
     border-bottom: 1px solid ${props => props.theme.colors.pinkishGrey};
   }
 `;
